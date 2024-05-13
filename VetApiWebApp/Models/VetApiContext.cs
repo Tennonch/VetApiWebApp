e36e4f94325c236f0366d171d985ceed4c65718f
Tennonch <rudenkiy.pasha@gmail.com>
2024-05-13 08:44:30 +0300

    Add project files.

---
+++ VetApiWebApp/Models/VetApiContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VetApiWebApp.Models
+{
+    public class VetApiContext : DbContext
+    {
+        public virtual DbSet<Patient> Patients { get; set; }
+
+        public virtual DbSet<Visit> Visits { get; set; }
+
+        public virtual DbSet<Doctor> Doctors { get; set; }
+
+        public virtual DbSet<Service> Services { get; set; }
+
+        public virtual DbSet<VisitService> VisitServices { get; set; }
+
+        public VetApiContext(DbContextOptions<VetApiContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ VetApiWebApp/Models/VisitService.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetApiWebApp.Models
+{
+    public class VisitService
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Visit")]
+        public int VisitId { get; set; }
+        public Visit Visit { get; set; }
+
+        [ForeignKey("Service")]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+    }
+}
